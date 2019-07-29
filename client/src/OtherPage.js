30acac6d426c0df2e56d66c7b477c2f4ce29d074
arnabkc <arnabc169@gmail.com>
2019-07-29 23:46:08 +0530

    Addedd client code, server code and docker files

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+            Test page for HTML5 Push message demonstration.
+            <Link to="/">Go Back to Home page</Link>
+        </div>
+    );
+};
